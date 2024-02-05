1572124f4efe821e29848fb8c646b37578182b19
Lev Shuster <shusterl@carleton.edu>
2024-02-05 15:20:08 -0600

    try removing english requirment

--- Website/XAI-Carleton-Comps-2024/docusaurus.config.ts
+++ Website/XAI-Carleton-Comps-2024/docusaurus.config.ts
@@ -28,10 +28,10 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en'],
+  // },
 
   presets: [
     [
