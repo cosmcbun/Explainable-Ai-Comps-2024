a51d7cd4e8b4f25c41223ee5a3d0333e88952231
Chris Melville <chrismelville@MacBook-Pro-135.local>
2024-02-07 16:36:07 -0600

    polar bear 60

--- resnet/anchoring/animal_anchoring.py
+++ resnet/anchoring/animal_anchoring.py
@@ -79,7 +79,7 @@ def explain(image, images_location):
 
 
 images_location = "../../../animal_images"
-image_name = "dog-shiba_inu-92.jpg"
+image_name = "dog-great_pyrenees-53.jpg"
 image = transform_image(images_location + "/" + image_name)
 
 
