5148d840b6ee44ee6414630cab4f1e3aacb756ae
Chris Melville <chrismelville@MacBook-Pro-135.local>
2024-02-07 16:31:48 -0600

    shiba inu 0.6

--- resnet/anchoring/animal_anchoring.py
+++ resnet/anchoring/animal_anchoring.py
@@ -74,12 +74,12 @@ def draw_anchor(segments, explanation, image):
 #image is a skimage
 def explain(image, images_location):
     explainer = anchor_image.AnchorImage(images_location, transform_img_fn=transform_images)
-    segments, explanation = explainer.explain_instance(image, predict, threshold=0.96)
+    segments, explanation = explainer.explain_instance(image, predict, threshold=0.6)
     return draw_anchor(segments, explanation, image)
 
 
 images_location = "../../../animal_images"
-image_name = "cat-birman-16.jpg"
+image_name = "shiba_inu-92.jpg"
 image = transform_image(images_location + "/" + image_name)
 
 
