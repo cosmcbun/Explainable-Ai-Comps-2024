655f9a7dea7a2f7d38b2d2dfabfb8b8fc70a9c51
Adrian Boskovic <52258788+boskovica@users.noreply.github.com>
2024-02-12 13:35:20 -0600

    Update index.tsx

--- Website/XAI-Carleton-Comps-2024/src/components/HomepageFeatures/index.tsx
+++ Website/XAI-Carleton-Comps-2024/src/components/HomepageFeatures/index.tsx
@@ -10,7 +10,7 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Anchor',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -20,7 +20,7 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'LIME',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -30,7 +30,7 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Shapley values',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
