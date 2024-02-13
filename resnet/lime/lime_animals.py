bea92ca74299c19d277c73760ec498dae2d541ae
Adrian Boskovic <52258788+boskovica@users.noreply.github.com>
2024-02-13 14:58:39 -0600

    Add files via upload

---
+++ resnet/lime/lime_animals.py
@@ -0,0 +1,121 @@
+#%%
+import matplotlib.pyplot as plt
+from PIL import Image
+import torch.nn as nn
+import numpy as np
+import os, json
+
+import torch
+from torchvision import models, transforms
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import glob
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+
+# Documentation from helpful LIME-Pytorch tutorial
+# https://github.com/marcotcr/lime/blob/master/doc/notebooks/Tutorial%20-%20images%20-%20Pytorch.ipynb
+
+# Set GPU device
+print(torch.cuda.is_available())
+device = torch.device("cuda:0")
+
+def get_image(path):
+    with open(os.path.abspath(path), 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+
+# resize and take the center part of image to what our model expects
+def get_input_transform():
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+    transf = transforms.Compose([
+        #transforms.Resize((256, 256)),
+        #transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    return transf
+
+def get_input_tensors(img):
+    transf = get_input_transform()
+    # unsqeeze converts single image to batch of 1
+    return transf(img).unsqueeze(0)
+
+# train the model
+pretrained_weights = models.ResNet18_Weights.IMAGENET1K_V1
+model = models.resnet18(weights=pretrained_weights)
+
+# get the ImageNet labels to make our predictions
+idx2label, cls2label, cls2idx = [], {}, {}
+with open(os.path.abspath('./imagenet_class_index.json'), 'r') as read_file:
+    class_idx = json.load(read_file)
+    idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
+    cls2label = {class_idx[str(k)][0]: class_idx[str(k)][1] for k in range(len(class_idx))}
+    cls2idx = {class_idx[str(k)][0]: k for k in range(len(class_idx))}
+
+# transform/normalize the image
+def get_pil_transform():
+    transf = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop(224)
+    ])
+
+    return transf
+
+def get_preprocess_transform():
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+    transf = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    return transf
+
+pill_transf = get_pil_transform()
+preprocess_transform = get_preprocess_transform()
+
+# predict function
+def batch_predict(images):
+    model.eval()
+    batch = torch.stack(tuple(preprocess_transform(i) for i in images), dim=0)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    batch = batch.to(device)
+
+    logits = model(batch)
+    probs = F.softmax(logits, dim=1)
+    return probs.detach().cpu().numpy()
+
+#%%
+#Get one image
+my_image = get_image('./user_study_images/cat-british_shorthair-78.jpg')
+#plt.imshow(my_image)
+
+# Image tensors
+img_t = get_input_tensors(my_image)
+model.eval()
+logits = model(img_t)
+probs = F.softmax(logits, dim=1)
+probs5 = probs.topk(5)
+
+image_explainer = lime_image.LimeImageExplainer()
+image_explanation = image_explainer.explain_instance(np.array(pill_transf(my_image)),
+                                         batch_predict, # classification function
+                                         top_labels=5,
+                                         hide_color=0,
+                                         num_samples=1000) # number of images that will be sent to classification function
+
+# Get top 5 predictions
+tuple((p,c, idx2label[c]) for p, c in zip(probs5[0][0].detach().numpy(), probs5[1][0].detach().numpy()))
+
+#%%
+# Apply mask on the image
+temp, mask = image_explanation.get_image_and_mask(image_explanation.top_labels[0], positive_only=False, num_features=5, hide_rest=False)
+img_boundry = mark_boundaries(temp/255.0, mask)
+plt.imshow(img_boundry)
+# %%
