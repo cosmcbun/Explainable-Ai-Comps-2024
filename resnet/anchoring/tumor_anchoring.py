ec5e8fc6ceb56c4d95b5a48c1bda99699b40079d
Chris Melville <chrismelville@MacBook-Pro-135.local>
2024-02-17 12:59:33 -0600

    switching back. also, healthy 228

--- resnet/anchoring/tumor_anchoring.py
+++ resnet/anchoring/tumor_anchoring.py
@@ -13,8 +13,8 @@ from skimage.segmentation import quickshift
 import skimage.io
 
 
-#model = torch.load("../../../mri_model_4class.pth", map_location="cpu")
-model = torch.load("../../../LevNetVGG16.pt", map_location="cpu")
+model = torch.load("../../../mri_model_4class.pth", map_location="cpu")
+#model = torch.load("../../../LevNetVGG16.pt", map_location="cpu")
 model.eval()
 
 if torch.cuda.is_available():
@@ -68,7 +68,7 @@ def explain(image, images_location):
 
 
 images_location = "../../../tumor_images"
-image_name = "sick-pituitary_tumor-422.jpg"
+image_name = "healthy-no_tumor-228.jpg"
 image = transform_image(images_location + "/" + image_name)
 
 
