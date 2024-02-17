4544bd586c8c824eb4050a1fb7cf97a1d9a3240c
Chris Melville <chrismelville@MacBook-Pro-135.local>
2024-02-17 12:41:08 -0600

    meningioma 252

--- resnet/anchoring/tumor_anchoring.py
+++ resnet/anchoring/tumor_anchoring.py
@@ -67,7 +67,7 @@ def explain(image, images_location):
 
 
 images_location = "../../../tumor_images"
-image_name = "healthy-no_tumor-191.jpg"
+image_name = "sick-meningioma_tumor-252.jpg"
 image = transform_image(images_location + "/" + image_name)
 
 
