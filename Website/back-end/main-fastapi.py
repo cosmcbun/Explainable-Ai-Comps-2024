caa48829b6ed2622baabb80737da01bced7662ef
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-02-22 19:37:14 -0600

    Added Flask prototype, modified explainers

---
+++ Website/back-end/main-fastapi.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, Depends
+import numpy as np
+import sys
+
+# Special imports:
+sys.path.append("../../MOOC/src")
+from mlp import MLPClassifier
+from data_loader import DataLoader
+from custom_student import Student
+from mooc_explainers import MOOCExplainers
+from resnet_explainers import ResnetExplainers
+
+
+#+ App/Model +#
+mooc_explainers = MOOCExplainers()
+resnet_explainers = ResnetExplainers()
+app = FastAPI() #! For a local demo, use "python -m uvicorn main-fastapi:app --reload"
+
+#+ API +#
+
+#* Root *#
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+#* MOOC *#
+@app.post("/predict_on_student/")
+def predict_on_student(student: Student):
+    x = student.encode()
+    y = mooc_explainers.model.predict(x)
+    return {"output": int(y[0])}
+
+@app.post("/shap_on_student/")
+def shap_on_student(student: Student):
+    return mooc_explainers.shap_on_student(student)
+
+@app.post("/lime_on_student/")
+def lime_on_student(student: Student):
+    return mooc_explainers.lime_on_student(student)
+
+@app.post("/anchor_on_student/")
+def anchor_on_student(student: Student):
+    return mooc_explainers.anchor_on_student(student)
+
+#* Resnet *#
+@app.post("/predict_on_animal/")
+def predict_on_animal(animal):
+    return resnet_explainers.predict_on_animal(animal)
+
+@app.post("/shap_on_animal/")
+def shap_on_animal(animal):
+    return resnet_explainers.shap_on_animal(animal)
+
+@app.post("/lime_on_animal/")
+def lime_on_animal(animal):
+    return resnet_explainers.lime_on_animal(animal)
+
+@app.post("/anchor_on_animal/")
+def anchor_on_animal(animal):
+    return resnet_explainers.anchor_on_animal(animal)
---
+++ Website/back-end/resnet_explainers.py
@@ -0,0 +1,22 @@
+import pickle    
+
+#! how the FUCK will we be passing in images???
+class Animal:
+    pass
+
+class ResnetExplainers:
+    def __init__(self):
+        pass
+    
+    def shap_on_animal(self, animal: Animal):
+        return {"shap": "Not implemented yet"}
+    
+    def lime_on_animal(self, animal: Animal):
+        return {"lime": "Not implemented yet"}
+    
+    def anchor_on_animal(self, animal: Animal):
+        return {"anchor": "Not implemented yet"}
+    
+    def predict_on_animal(self, animal: Animal):
+        return {"output": "Not implemented yet"}
+            
