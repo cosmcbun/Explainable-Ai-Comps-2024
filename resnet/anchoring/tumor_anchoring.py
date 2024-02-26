d4816970809f7b695fad00c3c251aaa24eb51383
Chris Melville <chrismelville@MacBook-Pro-135.local>
2024-02-26 09:15:54 -0600

    healthy 97 attempt 3

--- resnet/anchoring/tumor_anchoring.py
+++ resnet/anchoring/tumor_anchoring.py
@@ -68,7 +68,7 @@ def explain(image, images_location):
 
 
 images_location = "../../../tumor_images"
-image_name = "healthy-no_tumor.jpg"
+image_name = "healthy-no_tumor-97.jpg"
 image = transform_image(images_location + "/" + image_name)
 
 
