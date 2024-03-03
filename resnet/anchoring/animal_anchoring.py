99dc52f73f1e9c8933b301131c4275315a72cdfb
Chris Melville <chrismelville@MacBook-Pro-135.local>
2024-03-03 14:59:10 -0600

    trying to unbreak animal_anchoring even further again again

--- resnet/anchoring/animal_anchoring.py
+++ resnet/anchoring/animal_anchoring.py
@@ -24,15 +24,13 @@ if torch.cuda.is_available():
 
 
 tensorfy_image = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.ToPILImage(),
     transforms.ToTensor(),
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
 )])
 
-
-#tensorfy_image = transforms.ToTensor()
 
 def transform_image(path):
     #img = skimage.io.imread(path)
