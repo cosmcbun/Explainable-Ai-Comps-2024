f53ce8123dc19d59b3be72e68abd6366f57d976a
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-03-12 13:42:14 -0500

    Finalized MOOC Model

--- Website/XAI-Carleton-Comps-2024/src/components/interactivity/MLPClassifier.jsx
+++ Website/XAI-Carleton-Comps-2024/src/components/interactivity/MLPClassifier.jsx
@@ -3,12 +3,10 @@ import axios from 'axios';
 import "./style.css";
 
 export const MLPClassifier = () => {
-  const [numEvent, setNumEvent] = useState('');
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
-
   const handleChange = (e) => {
-    setNumEvent(e.target.value);
+    
   };
 
   useEffect(() => { // Query the server on page load to awaken it
@@ -17,18 +15,18 @@ export const MLPClassifier = () => {
 
   function handleClick() {
     try {
-      // POST request to server containing our ML model's input (" | 0" to clean unfilled values)
+      // POST request to server containing our ML model's input (" | DEFAULT" to clean unfilled values)
       axios.post(`https://xai-mlp.glitch.me/predict/`, {
-          viewed: document.getElementById("viewed").value | 0,
-          gender: document.getElementById("gender").value | 0,
-          grade: document.getElementById("grade").value | 0,
-          nevents: document.getElementById("nevents").value | 0,
-          ndays_act: document.getElementById("ndays_act").value | 0,
-          nplay_video: document.getElementById("nplay_video").value | 0,
-          nchapters: document.getElementById("nchapters").value | 0,
-          age: document.getElementById("age").value | 0,
-          votes: document.getElementById("votes").value | 0,
-          num_words: document.getElementById("num_words").value | 0
+          viewed: document.getElementById("viewed").value ? document.getElementById("viewed").value : 1,
+          gender: document.getElementById("gender").value ? document.getElementById("gender").value : 1,
+          grade: document.getElementById("grade").value ? document.getElementById("grade").value : 0.85,
+          nevents: document.getElementById("nevents").value ? document.getElementById("nevents").value : 4717,
+          ndays_act: document.getElementById("ndays_act").value ? document.getElementById("ndays_act").value : 43,
+          nplay_video: document.getElementById("nplay_video").value ? document.getElementById("nplay_video").value : 770,
+          nchapters: document.getElementById("nchapters").value ? document.getElementById("nchapters").value : 18,
+          age: document.getElementById("age").value ? document.getElementById("age").value : 27,
+          votes: document.getElementById("votes").value ? document.getElementById("votes").value : 2,
+          num_words: document.getElementById("num_words").value ? document.getElementById("num_words").value : 280
         }, 
         {
           headers: {
@@ -39,8 +37,6 @@ export const MLPClassifier = () => {
         // Return the array of probabilities [fail, pass, colors]
         let probs = response.data.Probability.map((prob) => prob.toFixed(3));
         let result = [probs[0], probs[1], ['#FF006F', '#0082FF']];
-        console.log("PROBABILITY: ", response.data.Probability)
-        console.log("RESULT:", result)
 
         // Colors based on winning result
         if (result[0] < result[1]) {
@@ -48,21 +44,6 @@ export const MLPClassifier = () => {
         }
         setResponse(result);
         setError(null);
-
-        // Animate the progress bars
-        let bars = document.getElementsByClassName("progressBar");
-        for (let i = 0; i < bars.length; i++) {
-          let width = 0;
-          let id = setInterval(frame, 10);
-          function frame() {
-            if (width >= response.data.Probability[i] * 100) {
-              clearInterval(id);
-            } else {
-              width++;
-              bars[i].style.width = width;
-            }
-          }
-        }
       });
 
     } catch (error) {
@@ -73,7 +54,7 @@ export const MLPClassifier = () => {
   };
 
   return (
-    <div>
+    <div key={new Date().getTime()}>
       <table>
         <tbody>
           <tr>
@@ -89,16 +70,16 @@ export const MLPClassifier = () => {
             <td>num_words</td>
           </tr>
           <tr>
-            <td><input id="viewed" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="gender" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="grade" type="number" onChange={handleChange} placeholder="0.0" style={{ width: '50px' }}/></td>
-            <td><input id="nevents" type="number"  onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="ndays_act" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="nplay_video" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="nchapters" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="age" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="votes" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
-            <td><input id="num_words" type="number" onChange={handleChange} placeholder="0" style={{ width: '50px' }}/></td>
+            <td><input id="viewed" type="number" onChange={handleChange} placeholder="1" style={{ width: '50px' }}/></td>
+            <td><input id="gender" type="number" onChange={handleChange} placeholder="1" style={{ width: '50px' }}/></td>
+            <td><input id="grade" type="number" onChange={handleChange} placeholder="0.85" style={{ width: '50px' }}/></td>
+            <td><input id="nevents" type="number"  onChange={handleChange} placeholder="4717" style={{ width: '50px' }}/></td>
+            <td><input id="ndays_act" type="number" onChange={handleChange} placeholder="43" style={{ width: '50px' }}/></td>
+            <td><input id="nplay_video" type="number" onChange={handleChange} placeholder="770" style={{ width: '50px' }}/></td>
+            <td><input id="nchapters" type="number" onChange={handleChange} placeholder="18" style={{ width: '50px' }}/></td>
+            <td><input id="age" type="number" onChange={handleChange} placeholder="27" style={{ width: '50px' }}/></td>
+            <td><input id="votes" type="number" onChange={handleChange} placeholder="2" style={{ width: '50px' }}/></td>
+            <td><input id="num_words" type="number" onChange={handleChange} placeholder="280" style={{ width: '50px' }}/></td>
           </tr>
         </tbody>
       </table>
@@ -111,15 +92,6 @@ export const MLPClassifier = () => {
           <svg style={{ width: '100%', height: '79px' }}>
             <text x="20" y="20">Prediction probabilities</text>
             <g>
-              {/* <rect x="120" className="progressBar" y="35" height="17" width={response[0] * 100} style={{ fill: response[2][0] }}></rect>
-              <rect x="120" y="35" height="17" width="100" fillOpacity="0" stroke="black"></rect>
-              <text x="225" y="49" fill="black" style={{ font: '14px tahoma, sans-serif' }}>{response[0]}</text>
-              <text x="110" y="49" fill="black" textAnchor="end" style={{ font: '14px tahoma, sans-serif' }}>Not Completed</text>
-              <rect x="120" className="progressBar" y="57" height="17" width={response[1] * 100} style={{ fill: response[2][1] }}></rect>
-              <rect x="120" y="57" height="17" width="100" fillOpacity="0" stroke="black"></rect>
-              <text x="225" y="71" fill="black" style={{ font: '14px tahoma, sans-serif' }}>{response[1]}</text>
-              <text x="110" y="71" fill="black" textAnchor="end" style={{ font: '14px tahoma, sans-serif' }}>Completed</text> */}
-
               <rect x="120" className="progressBar" y="35" height="17" width={response[0] * 100} stroke="black" style={{ fill: response[2][0] }}></rect>
               <text x={120 + response[0] * 100 + 5} y="49" fill="black" style={{ font: '14px tahoma, sans-serif' }}>{response[0]}</text>
               <text x="110" y="49" fill="black" textAnchor="end" style={{ font: '14px tahoma, sans-serif' }}>Not Completed</text>
